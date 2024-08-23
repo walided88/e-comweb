05097ac17ad170a9fadaee2cdae95c20ba5d45d9
walided88 <walideddaif@gmail.com>
2024-08-23 08:36:09 +0100

    Update socket.js

--- server/socket.js
+++ server/socket.js
@@ -8,40 +8,32 @@ const mongoDbUrl = process.env.MONGODB_URL; // Assurez-vous que cette variable e
 const setupSocket = (server) => {
     const io = socketIo(server, {
         cors: {
-            origin: "http://localhost:3000",
+            origin: 'https://ecom-chi-nine.vercel.app', // L'URL de ton site en production
             methods: ["GET", "POST"],
             credentials: true
         }
     });
 
-
-
     io.use((socket, next) => {
         const token = socket.handshake.auth.token;
         if (token) {
-            jwt.verify(token, jwtSecret, (err, decoded) => { // Replace 'xxxx' with your JWT secret
+            jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
                 if (err) {
                     return next(new Error("Authentication error"));
                 }
-                socket.user = decoded; // Attach user info to socket
+                socket.user = decoded;
                 next();
             });
         } else {
             next(new Error("Authentication error"));
         }
     });
+
     io.on('connection', (socket) => {
         console.log('A user connected:', socket.user);
-    
-        // Emit event to the client with the user data
+
         socket.emit('user-connected', socket.user);
-    
-        console.log('User mail:', socket.user?.mail);
-        socket.emit('user-mail', socket.user?.mail);
-    
-        console.log('User name:', socket.user?.name);
-        socket.emit('user-name', socket.user?.name);
-    
+
         socket.on('message', (message) => {
             const fullMessage = {
                 ...message,
@@ -51,15 +43,12 @@ const setupSocket = (server) => {
             };
             io.emit('message', fullMessage);
         });
-    
+
         socket.on('disconnect', () => {
             console.log('User disconnected');
             socket.emit('user-disconnected', 'User disconnected');
         });
     });
-    
-    
 };
-
 
 module.exports = setupSocket;
