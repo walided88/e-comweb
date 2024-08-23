39a1c367c46ac2d36a050dc32fdfb28696c5d7bd
walided88 <walideddaif@gmail.com>
2024-08-23 09:32:09 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,32 +1,55 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
 const http = require('http');
-const setupSocket = require('./socket'); // Importer la configuration de socket.io
-
-const userRoutes = require('./routes/userRoutes');
-const clientRoutes = require('./routes/clientRoutes');
-
+const socketIo = require('socket.io');
 const app = express();
-app.use(cors({
-    origin: "https://ecom-chi-nine.vercel.app", // Remplacez par l'origine de votre frontend
-    methods: ["GET", "POST"],
-    credentials: true
-}));
-app.use(cors()); // Activer CORS pour toutes les routes
-app.use(express.json());
-
 const server = http.createServer(app);
+const jwt = require('jsonwebtoken');
+const jwtSecret = process.env.JWT_SECRET;
+const io = socketIo(server, {
+    cors: {
+        origin: "https://ecom-chi-nine.vercel.app",
+        methods: ["GET", "POST"],
+        credentials: true
+    }
+});
 
-setupSocket(server);
+// Middleware pour gérer le token JWT
+io.use((socket, next) => {
+    const token = socket.handshake.auth.token;
+    if (token) {
+        jwt.verify(token, jwtSecret, (err, decoded) => {
+            if (err) {
+                return next(new Error("Authentication error"));
+            }
+            socket.user = decoded;
+            next();
+        });
+    } else {
+        next(new Error("Authentication error"));
+    }
+});
 
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('Error connecting to MongoDB:', err.message));
+// Gestion des connexions
+io.on('connection', (socket) => {
+    console.log('A user connected:', socket.user);
 
-app.use('/users', userRoutes);
-app.use('/clients', clientRoutes);
+    socket.on('message', (message) => {
+        const fullMessage = {
+            ...message,
+            mail: socket.user.mail,
+            name: socket.user.name,
+            createdAt: new Date().toISOString()
+        };
+        io.emit('message', fullMessage);
+    });
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+// Démarrer le serveur
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
