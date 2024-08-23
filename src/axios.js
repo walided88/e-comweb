5ecc5db2adfd0f2634f940b1f3107b65409aab4c
walided88 <walideddaif@gmail.com>
2024-08-23 10:58:02 +0100

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 // Déterminer l'URL de base en fonction de l'environnement
-const baseURL = 'https://ecom-chi-nine.vercel.app'
+const baseURL = process.env.NODE_ENV === 'production'
+    ? 'https://ecom-chi-nine.vercel.app' // URL en production
+    : 'http://localhost:5000'; // URL en développement
+
 // Instance pour les requêtes liées aux utilisateurs
 const instanceUsers = axios.create({
     baseURL: `${baseURL}/users`, // Base URL pour les utilisateurs
