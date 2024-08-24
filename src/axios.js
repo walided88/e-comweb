ef2960444c7a099e27581bda2529e47512931193
walided88 <walideddaif@gmail.com>
2024-08-24 10:53:15 +0100

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -3,12 +3,12 @@ import axios from 'axios';
 
 // Instance pour les requêtes liées aux utilisateurs
 const instanceUsers = axios.create({
-    baseURL: 'http://localhost:5000/users', // Base URL pour les utilisateurs
+    baseURL: 'http://105.71.7.5/users', // Base URL pour les utilisateurs
 });
 
 // Instance pour les requêtes liées aux clients
 const instanceClients = axios.create({
-    baseURL: 'http://localhost:5000/clients', // Base URL pour les clients
+    baseURL: 'http://105.71.7.5/clients', // Base URL pour les clients
 });
 
 // Exportation des deux instances
