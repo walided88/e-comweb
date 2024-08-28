6ced78808c1d239508956a79c6858c4397662b48
walided88 <walideddaif@gmail.com>
2024-08-28 15:36:58 +0200

    Update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,6 +5,7 @@ import { instanceClients } from '../axios';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import { useSelector, useDispatch } from 'react-redux';
+import Loader from './Loader';
 
 const ErrorMessage = styled.p`
   color: red;
@@ -25,6 +26,7 @@ function Form({totaleP}) {
   const dispatch = useDispatch();
   const prodSelecteds = useSelector((state) => state.user.productNumber);
   const [price, setPrice] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
 
 
   function multiplyArrayElements(arr, multiplier) {
@@ -38,6 +40,8 @@ function Form({totaleP}) {
   }, [list]);
 
   const handleSubmit = async (event) => {
+    setIsLoading(true)
+
     event.preventDefault();
     setError('');
     try {
@@ -80,6 +84,8 @@ function Form({totaleP}) {
 
   return (
     <form onSubmit={handleSubmit} style={styles.form}>
+     {isLoading && <div>    <Loader /></div>}
+
       {error && <ErrorMessage>{error}</ErrorMessage>}
       <h2 style={styles.title}>Formulaire</h2>
 
