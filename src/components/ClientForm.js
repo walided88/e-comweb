788e66efc3a163893027afb50c8c6f77a62021a3
walided88 <walideddaif@gmail.com>
2024-09-03 00:19:21 +0200

    Update ClientForm.js

--- src/components/ClientForm.js
+++ src/components/ClientForm.js
@@ -2,16 +2,20 @@
 import React, { useEffect, useState } from 'react';
 import {  instanceClients } from '../axios';
 import DataTable from './DataTable';
+import Loader from './Loader';
 
 const Home = () => {
     const [users, setUsers] = useState([]);
     const headerss = ['Nom', 'Email', 'Num','Adresse','Ville',"commandes"];
+    const [isLoading, setIsLoading] = useState('false');
 
     useEffect(() => {
         const fetchUsers = async () => {
+            setIsLoading(true)
             try {
                 const response = await instanceClients.get('/');
                  setUsers(response.data);
+                 setIsLoading(false)
 
             } catch (error) {
                console.error('Error fetching users:', error);
@@ -25,10 +29,11 @@ console.log(users);
 
       <div >
 
-        <div >
+<p style={{marginLeft:"33%"}}>
+        {isLoading &&  <Loader />}
+        </p>
 
         <DataTable  headers={headerss} data={users} />
-        </div>
 
  
       
