9ffd150bfdf55696e8ac9e75e5aeb9906d466655
walided88 <walideddaif@gmail.com>
2024-11-14 14:44:06 +0100

    Update server.js

--- labo/server/server.js
+++ labo/server/server.js
@@ -3,25 +3,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
-const http = require('http');
 
 const userRoutes = require('./routes/userRoutes');
 const clientRoutes = require('./routes/clientRoutes');
 const messageRoutes = require('./routes/messageRoutes');
 
 const app = express();
-app.use(cors({
-    origin: '*', // Autorise toutes les origines, pour les tests
 
-    methods: ["GET", "POST","PUT"],
-    // allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true
-}));
 app.use(cors()); // Activer CORS pour toutes les routes
 app.use(express.json());
-
-const server = http.createServer(app); // Créer le serveur HTTP
-
 
 // Connexion à MongoDB
 mongoose.connect(process.env.MONGO_URI)
