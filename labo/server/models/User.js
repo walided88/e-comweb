6e4dddf989d0d8b6f87db665a9b270eef296d564
walided88 <walideddaif@gmail.com>
2024-11-15 00:32:20 +0100

    Update User.js

--- labo/server/models/User.js
+++ labo/server/models/User.js
@@ -31,16 +31,25 @@ const userSchema = new mongoose.Schema({
     }
 });
 
-// Pre-save middleware to hash the password before saving
+// Middleware "pre-save" pour hacher le mot de passe avant de sauvegarder un document utilisateur
 userSchema.pre('save', async function (next) {
+    // Vérifie si le champ "password" a été modifié
+    // Si le mot de passe n'a pas été modifié (par exemple lors d'une mise à jour de profil sans changer le mot de passe), on passe à l'étape suivante
     if (!this.isModified('password')) return next();
+
     try {
+        // Hachage du mot de passe avec bcrypt en utilisant un "salt round" de 10
+        // Le "salt round" détermine la complexité du processus de hachage (10 est un bon compromis entre sécurité et performance)
         this.password = await bcrypt.hash(this.password, 10);
+
+        // Passe à l'étape suivante après avoir haché le mot de passe
         next();
     } catch (err) {
+        // En cas d'erreur lors du hachage, transmettre l'erreur au middleware suivant
         next(err);
     }
 });
+
 
 const User = mongoose.model('User', userSchema);
 
